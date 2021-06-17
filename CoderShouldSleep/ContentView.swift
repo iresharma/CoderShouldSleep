38fc71848b1e63844f01611ebae313b62e3570d0
Iresharma <iresh.sharma8@gmail.com>
2021-06-17 23:21:15 +0530

    almost done

--- CoderShouldSleep/ContentView.swift
+++ CoderShouldSleep/ContentView.swift
@@ -6,11 +6,81 @@
 //
 
 import SwiftUI
+import EventKit
 
 struct ContentView: View {
+    
+    @State private var wakeDate = Date()
+    @State private var amountSleep = 8.0
+    @State private var nCoffee = 2
+    @State private var err = false
+    @State private var calcDone = false
+    
+    @State private var sleepDate = DateComponents()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        DatePicker("When do you wanna wake up ?", selection: $wakeDate, in: Date()...Date().addingTimeInterval(86400), displayedComponents: .hourAndMinute)
+                        Stepper(value: $amountSleep, in: 4...12, step: 0.25) {
+                            Text("Sleep for \(amountSleep, specifier: "%g") hours")
+                        }
+                    }
+                    Section(header: Text("No. of coffee cups per day")) {
+                        Stepper(value: $nCoffee, in: 1...20) {
+                            Text(nCoffee > 1 ? "\(nCoffee) cups" : "1 cup").fontWeight(.black)
+                        }
+                    }
+                }
+                if calcDone {
+                    Text("You should sleep at:")
+                        .font(.title3)
+                        .padding(.bottom)
+                    Text("\(sleepDate.hour ?? 00) : \(sleepDate.minute ?? 00)")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(.green)
+                        .padding(.bottom)
+                    Button(action: alarm, label: {
+                        HStack {
+                            Image(systemName: "hourglass.badge.plus")
+                            Text("Add Alarm")
+                        }
+                    })
+                }
+            }
+            .navigationTitle("BetterSleep")
+            .navigationBarItems(trailing: Button(action: self.calc, label: {
+                Text("Calc")
+            }))
+        }.alert(isPresented: $err) {
+            Alert(title: Text("Error"), message: Text("Some error occures"), dismissButton: .default(Text("ok")))
+        }
+    }
+    
+    func calc() {
+        let model = BetterSleep()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeDate)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        let seconds = hour + minute
+        do {
+            let prediction = try model.prediction(input: BetterSleepInput(wake: Double(seconds), estimatedSleep: amountSleep, coffee: Double(nCoffee)))
+            
+            let sleepTime = wakeDate - prediction.actualSleep
+            
+            sleepDate = Calendar.current.dateComponents([.hour, .minute], from: sleepTime)
+            calcDone = true
+        } catch {
+            err = true
+        }
+        
+    }
+    
+    func alarm() {
+        
     }
 }
 
