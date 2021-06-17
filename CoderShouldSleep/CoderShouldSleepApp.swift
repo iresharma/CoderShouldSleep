67344ad30016db8236d188bf5a1da1928de40a6b
Iresharma <iresh.sharma8@gmail.com>
2021-06-17 18:41:45 +0530

    Initial Commit

---
+++ CoderShouldSleep/CoderShouldSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoderShouldSleepApp.swift
+//  CoderShouldSleep
+//
+//  Created by iresh sharma on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CoderShouldSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
